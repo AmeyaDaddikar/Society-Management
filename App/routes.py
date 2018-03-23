882ef71b1f2779cb1207121e45b68202f2eb91a7
AmeyaDaddikar <ameyadaddikar@gmail.com>
2018-03-23 17:08:20 +0530

    Added a base.html to be the base template

--- App/routes.py
+++ App/routes.py
@@ -1,6 +1,10 @@
 from . import app
 from flask import render_template, request
 
+# OPTIONAL/ REQUIRED ARGUMENT LIST
+# 1. title (base.html) : Displays the title of the page
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
