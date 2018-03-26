c19e0877cb87b4eb7cb89e736fb1b3925c87763a
AmeyaDaddikar <ameyadaddikar@gmail.com>
2018-03-27 02:45:15 +0530

    Added a server side form validation mechanism to the page. To-do : 1. Send the loginError as POST rather than GET

--- App/routes.py
+++ App/routes.py
@@ -1,5 +1,5 @@
 from . import app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, make_response
 
 # OPTIONAL/ REQUIRED ARGUMENT LIST
 # 1. title (base.html) : Displays the title of the page
@@ -8,4 +8,29 @@ from flask import render_template, request
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	try:
+		loginError = request.args.get("loginError")
+	except:
+		print("No Login Error")
+		loginError = None
+	
+	print(loginError)
+	return render_template('index.html',loginError=loginError)
+
+@app.route('/loginCheck', methods=['POST'])
+def login():
+	accName  = request.form['accName']
+	password = request.form['pwd']
+	
+	#validate accName and password here
+	#replace this condition
+	if accName != 'Ameya':
+		return redirect(url_for('index', loginError=True))
+		
+	return redirect(url_for('userDashboard'))
+
+@app.route('/dashboard', methods=['GET'])
+
+def userDashboard():
+	return render_template('404.html')
+
---
+++ App/templates/404.html
@@ -0,0 +1,11 @@
+{% extends "base.html" %}
+
+{% block headIncludes %}
+	<h1>404 Error</h1>
+	<h2>An error occured in handling your previous reuqest.</h2>
+	
+{% endblock %}
+
+{% block content %}
+
+{% endblock %}
