b540c180d508559b1f67e72d172bc52b88551fa4
Ameya Daddikar <ameyadaddikar@gmail.com>
2020-04-18 12:47:50 +0530

    Minor changes for deployment

--- App/dbconnect.py
+++ App/dbconnect.py
@@ -1,10 +1,11 @@
-import MySQLdb
+import pymysql
+import os
 
-HOST     = 'localhost'
-USER     = 'root'
-PASSWD   = 'c1o2l3d4'
-DATABASE = 'society_mysqldb'
+HOST     = os.environ.get('DB_HOST') or 'localhost'
+USER     = os.environ.get('DB_USER') or 'root'
+PASSWD   = os.environ.get('DB_PASSWD') or 'c1o2l3d4'
+DATABASE = os.environ.get('DB_DATABASE') or 'society_mysqldb'
 def connection():
-	conn = MySQLdb.connect(host = HOST, user = USER, passwd = PASSWD, db = DATABASE)
+	conn = pymysql.connect(host = HOST, user = USER, passwd = PASSWD, db = DATABASE)
 	cursor = conn.cursor()
 	return conn, cursor
--- start.py
+++ start.py
@@ -1,1 +1,3 @@
 from App import app
+if __name__ == '__main__':
+  app.run()
