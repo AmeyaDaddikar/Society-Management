eeae4ef32fadbf6f7372f9b54b130d837b226844
Ameya Daddikar <ameyadaddikar@gmail.com>
2020-04-19 21:48:58 +0530

    Added Environment variables as instructed in AWS Elastic Beanstalk guide https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/using-features.managing.db.html

--- App/dbconnect.py
+++ App/dbconnect.py
@@ -1,11 +1,12 @@
 import pymysql
 import os
 
-HOST     = os.environ.get('DB_HOST') or 'localhost'
-USER     = os.environ.get('DB_USER') or 'root'
-PASSWD   = os.environ.get('DB_PASSWD') or 'c1o2l3d4'
-DATABASE = os.environ.get('DB_DATABASE') or 'society_mysqldb'
+HOST     = os.environ.get('RDS_HOSTNAME') or 'localhost'
+USER     = os.environ.get('RDS_USERNAME') or 'root'
+PASSWD   = os.environ.get('RDS_PASSWORD') or 'c1o2l3d4'
+DATABASE = os.environ.get('RDS_DB_NAME') or 'society_mysqldb'
+PORT     = os.environ.get('RDS_PORT') or 3306
 def connection():
-	conn = pymysql.connect(host = HOST, user = USER, passwd = PASSWD, db = DATABASE)
+	conn = pymysql.connect(host = HOST, user = USER, passwd = PASSWD, db = DATABASE, port = PORT)
 	cursor = conn.cursor()
 	return conn, cursor
